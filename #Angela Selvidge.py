7e87d46aa44c8c341fb416d12b4e52dd16ada471
angselvidge <126904651+angselvidge@users.noreply.github.com>
2024-03-18 02:11:11 +0000

    1st commit

---
+++ #Angela Selvidge.py
@@ -0,0 +1,21 @@
+#Angela Selvidge    
+#March 15th 2024 @6:24pm
+#Prof. Shane Thomas
+
+
+def exists_in_domain (domain_set, test_element): # for loop for bullet point #1
+    for element in domain_set:   
+        if element ==  test_element        # creates both domains == to each other
+        return True
+    else:
+        return False
+    
+def sets_in_domain (domain_set, comp_test): # for loop for bullet point #2 
+     for element_in_domain_set      
+        if not comp_test:       #go back to w3 schools section 3 for clarification
+            return False
+        else: 
+            return True
+
+
+                
